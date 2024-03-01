574d6b28e9f92246a75be23b9fc0b739b0e9787a
tiburcioMartim <tiburciomartim@gmail.com>
2024-03-01 08:49:22 -0300

    add solution IMC refatoration

--- 04-exe02-imc/script.js
+++ 04-exe02-imc/script.js
@@ -1,4 +1,4 @@
-/*
+/* Descrition 
 2) O IMC - Indice de Massa Corporal é um critério da Organização Mundial de Saúde para dar uma indicação sobre a condição de peso de uma pessoa adulta.
 
 Formula do IMC:
@@ -13,27 +13,33 @@ IMC em adultos Condição:
 - Entre 30 e 40: Obeso;
 - Acima de 40: Obesidade Grave;
 */
-function imc() {
-    const myWeight = 78; // Math.random() * 91;
-    const myHeight = 1.75 // Math.random() * (2.4 - 1);
-    return myWeight / Math.pow(myHeight,2)
+
+//Calcula o IMC e retorna o valor dentro da function typeImc()
+function calculatorImc(w, h) {
+    vImc = w / Math.pow(h, 2);
+    return typeImc(vImc.toFixed(2));
 }
 
-const vImc = imc().toFixed(2);
+//Retorna o tipo de IMC 
+function typeImc(vImc) {
+    if (vImc < 18.5) {
+        return `IMC ${vImc}, está abaixo de 18.5: Abaixo do peso`;
 
-if (vImc < 18.5) {
-    console.log(`IMC ${vImc}, está abaixo de 18.5: Abaixo do peso`);
+    } else if (vImc >= 18.5 && vImc <= 25) {
+        return `IMC ${vImc}, está entre 18.5 e 25: Peso normal`;
 
-} else if (vImc >= 18.5 && vImc <= 25) {
-    console.log(`IMC ${vImc}, está entre 18.5 e 25: Peso normal`);
+    } else if (vImc >= 25 && vImc <= 30) {
+        return `IMC ${vImc}, está entre 25 e 30: Acima do peso`;
 
-} else if (vImc >= 25 && vImc <= 30) {
-    console.log(`IMC ${vImc}, está entre 25 e 30: Acima do peso`);
+    } else if (vImc >= 30 && vImc <= 40) {
+        return `IMC ${vImc}, está entre 30 e 40: Obeso`;
 
-} else if (vImc >= 30 && vImc <= 40) {
-    console.log(`IMC ${vImc}, está entre 30 e 40: Obeso`);
+    } else {
+        return `IMC ${vImc}, está acima de 40: Obesidade Grave`;
+    };
+};
 
-} else {
-    console.log(`IMC ${vImc}, está acima de 40: Obesidade Grave`);
+const weight = 74;
+const height = 1.71;
 
-}
+console.log(calculatorImc(weight, height));
